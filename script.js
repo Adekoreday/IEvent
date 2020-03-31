c28e70780e6228761aa2a2638c9aa4ef75ff7450
Adekoreday <adekoredead@ha-shem.com>
2020-03-31 05:10:01 +0100

    get all event on application load

--- script.js
+++ script.js
@@ -14,8 +14,48 @@ $("#login-btn").click(function(){
     $.ajax({ url: "https://vanhackacton.herokuapp.com/api/v1/events/readall",
            method: 'GET',
             success: function(response){
-               alert("done");
                console.log(response);
+             Object.keys(response.data).forEach(function(element, i) {
+                var item = document.createElement('div');
+                item.className = 'card';
+                item.style.width = '18rem';
+                item.style.margin = '4rem';
+
+                var image = document.createElement('img');
+                image.className = 'card-img-top';
+                image.src = response.data[element].imageUrl;
+                item.appendChild(image);
+
+                var content = document.createElement('div');
+                content.className = 'card-body';
+
+                var cardTitle = document.createElement('h5');
+                cardTitle.className = 'card-title';
+                cardTitle.innerHTML= response.data[element].name;
+
+                var location = document.createElement('div');
+                var date =document.createElement('div');
+                
+                location.innerHTML = response.data[element].location;
+                date.innerHTML= new Date(response.data[element].date).toString().slice(0, 15);
+                
+                var button =document.createElement('a');
+                button.className='btn btn-primary';
+                button.innerHTML='Book Event';
+                button.style.color='#FFFFFF';
+                button.style.marginTop = '10px';
+                button.id = response.data[element].id;
+
+                content.appendChild(cardTitle);
+                content.appendChild(location);    
+                content.appendChild(date);
+                content.appendChild(button);
+                item.appendChild(content);
+
+                
+                $('.event__list').append(item);
+               });
             }});
     });
 
+    
